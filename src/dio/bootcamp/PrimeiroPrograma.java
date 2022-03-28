7f8dc5f7c115131139e22d078fe34ecda7dc3a0a
Talita Rocha <rochatalita86@gmail.com>
2022-03-28 18:33:14 -0300

    first commit

---
+++ src/dio/bootcamp/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package dio.bootcamp;
+
+import dio.bootcamp.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 8;
+		System.out.println("Hello word! Nota " + (a+b));*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
